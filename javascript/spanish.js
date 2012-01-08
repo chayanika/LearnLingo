6e160a275bd82c893cb19d68ec31abce5b473447
Chayanika Ghosh <chayanika.ghosh24588@gmail.com>
2012-01-08 17:25:28 +0530

    added more images and the javascriptfile

---
+++ javascript/spanish.js
@@ -0,0 +1,48 @@
+(function() {
+	LLSpanish = {
+		business : {
+			words : {
+				english : ["word1", "word2", "word3"],
+				spanish : ["spword1", "spword1", "spword2"],
+				images : ["../images/word1", "../images/word2", "../images/word3"],
+				description : ["description1", "description2", "description3"]	
+			},
+			phrases : {
+				english : ["word1", "word2", "word3"],
+				spanish : ["spword1", "spword1", "spword2"],
+				images : ["../images/word1", "../images/word2", "../images/word3"],
+				description : ["description1", "description2", "description3"]		
+			},
+			conversations : {
+				english : ["word1", "word2", "word3"],
+				spanish : ["spword1", "spword1", "spword2"],
+				images : ["../images/word1", "../images/word2", "../images/word3"],
+				description : ["description1", "description2", "description3"]		
+			}
+		},
+		tourist : {
+			food : {
+				
+			},
+			places : {
+				
+			},
+			conversations : {
+				
+			}
+		},
+		everyday : {
+			words : {
+				
+			},
+			phrases : {
+				
+			},
+			conversations : {
+				
+			}
+		}
+	}	
+
+})();
+
