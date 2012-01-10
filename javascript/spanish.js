b06193534a766b440897d35085022cc9941e8e81
Chayanika Ghosh <chayanika.ghosh24588@gmail.com>
2012-01-10 19:21:26 +0530

    content addintion in .js file

--- javascript/spanish.js
+++ javascript/spanish.js
@@ -2,16 +2,16 @@
 	LLSpanish = {
 		business : {
 			words : {
-				english : ["word1", "word2", "word3"],
-				spanish : ["spword1", "spword1", "spword2"],
-				images : ["../images/word1", "../images/word2", "../images/word3"],
-				description : ["description1", "description2", "description3"]	
+				english : ["I want", "I need", "to go"],
+				spanish : ["(Yo) Quiero", "Yo necesito", "ir"],
+				images : ["../images/i-want-my-toy-back.jpg", "../images/i-need.jpg", "../images/togo.jpg"],
+				description : ["description1", "description2", "Thr pronunciation of 'r' is like 'd' in Spanish"]	
 			},
 			phrases : {
-				english : ["word1", "word2", "word3"],
-				spanish : ["spword1", "spword1", "spword2"],
-				images : ["../images/word1", "../images/word2", "../images/word3"],
-				description : ["description1", "description2", "description3"]		
+				english : ["You welcome", "Excuse me", "Do you speak Spanish?"],
+				spanish : ["De nada", "Perdon", "Hablo un poquito"],
+				images : ["../images/youre-welcome.gif", "../images/excuseme.jpg", "../images/doUSpeakSpanish.jpg"],
+				description : ["description1", "If you need to get by someone or leave a group of people: con permiso", "description3"]		
 			},
 			conversations : {
 				english : ["word1", "word2", "word3"],
@@ -22,23 +22,46 @@
 		},
 		tourist : {
 			food : {
+				english : ["Soup", "Water", "Juice"],
+				spanish : ["Sopa", "Agua", "Jugo"],
+				images : ["../images/i-want-my-toy-back.jpg", "../images/i-need.jpg", "../images/togo.jpg"],
+				description : ["description1", "description2", "The pronunciation of 'r' is like 'd' in Spanish"]	
 				
 			},
 			places : {
+				english : ["Place", "Excuse me", "Do you speak Spanish?"],
+				spanish : ["Lugar", "Perdon", "Hablo un poquito"],
+				images : ["../images/youre-welcome.gif", "../images/excuseme.jpg", "../images/doUSpeakSpanish.jpg"],
+				description : ["description1", "If you need to get by someone or leave a group of people: con permiso", "description3"]	
 				
 			},
 			conversations : {
+				english : ["I don't understand", "word2", ""],
+				spanish : ["No entiendo", "spword1", "spword2"],
+				images : ["../images/word1", "../images/word2", "../images/word3"],
+				description : ["description1", "description2", "description3"]	
 				
 			}
 		},
 		everyday : {
 			words : {
+				english : ["to order", "to drink", "to cook"],
+				spanish : ["ordenar/pedir", "tomar/beber", "cocinar/cocer"],
+				images : ["../images/toOrder.jpg", "../images/toDrink.jpg", "../images/toCook.jpg"],
+				description : ["ordenar is more common. 'Pedir' also means 'to ask for'", "'tomar' is used more commonly than 'beber'", "Both words mean same thing. Speakers in different region will use one or the other"]	
 				
 			},
-			phrases : {
+			phrases : {english : ["You welcome", "Excuse me", "Do you speak Spanish?"],
+				spanish : ["De nada", "Perdon", "Hablo un poquito"],
+				images : ["../images/youre-welcome.gif", "../images/excuseme.jpg", "../images/doUSpeakSpanish.jpg"],
+				description : ["description1", "If you need to get by someone or leave a group of people: con permiso", "description3"]	
 				
 			},
 			conversations : {
+				english : ["word1", "word2", "Do you speak Spanish?"],
+				spanish : ["spword1", "spword1", "¿Hablo un poquito?"],
+				images : ["../images/word1", "../images/word2", "../images/word3"],
+				description : ["description1", "description2", "description3"]	
 				
 			}
 		}
