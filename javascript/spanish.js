309964ace7083626c196eab06c79c3a60135deb7
Chayanika Ghosh <chayanika.ghosh24588@gmail.com>
2012-01-11 09:47:05 +0530

    modified the spanish.js

--- javascript/spanish.js
+++ javascript/spanish.js
@@ -5,17 +5,17 @@
 				english : ["I want", "I need", "to go"],
 				spanish : ["(Yo) Quiero", "Yo necesito", "ir"],
 				images : ["../images/i-want-my-toy-back.jpg", "../images/i-need.jpg", "../images/togo.jpg"],
-				description : ["description1", "description2", "Thr pronunciation of 'r' is like 'd' in Spanish"]	
+				description : ["You must have notice (Yo) which means 'I' is in paranthesis. In Spanish (Yo) is optional and is used only when emphasized. It is been generally left out in everyday speech", " ", "The pronunciation of 'r' is like 'd' in Spanish"]	
 			},
 			phrases : {
 				english : ["You welcome", "Excuse me", "Do you speak Spanish?"],
 				spanish : ["De nada", "Perdon", "Hablo un poquito"],
 				images : ["../images/youre-welcome.gif", "../images/excuseme.jpg", "../images/doUSpeakSpanish.jpg"],
-				description : ["description1", "If you need to get by someone or leave a group of people: con permiso", "description3"]		
+				description : [" ", "If you need to get by someone or leave a group of people: con permiso", " "]		
 			},
 			conversations : {
-				english : ["word1", "word2", "word3"],
-				spanish : ["spword1", "spword1", "spword2"],
+				english : ["Thank you very much.", "We appreciate your business.", "word3"],
+				spanish : ["Muchas gracias.", "Apreciamos hacer negocios con usted.", "spword2"],
 				images : ["../images/word1", "../images/word2", "../images/word3"],
 				description : ["description1", "description2", "description3"]		
 			}
@@ -24,22 +24,22 @@
 			food : {
 				english : ["Soup", "Water", "Juice"],
 				spanish : ["Sopa", "Agua", "Jugo"],
-				images : ["../images/i-want-my-toy-back.jpg", "../images/i-need.jpg", "../images/togo.jpg"],
-				description : ["description1", "description2", "The pronunciation of 'r' is like 'd' in Spanish"]	
+				images : ["../images/soup.jpg", "../images/water.jpg", "../images/Juice.jpg"],
+				description : [" ", " ", " "]	
 				
 			},
 			places : {
-				english : ["Place", "Excuse me", "Do you speak Spanish?"],
-				spanish : ["Lugar", "Perdon", "Hablo un poquito"],
+				english : ["Place", "Where is the subway?", "Is the tip included?"],
+				spanish : ["Lugar", "¿Dónde está el metro?", "¿Incluye la propina?"],
 				images : ["../images/youre-welcome.gif", "../images/excuseme.jpg", "../images/doUSpeakSpanish.jpg"],
 				description : ["description1", "If you need to get by someone or leave a group of people: con permiso", "description3"]	
 				
 			},
 			conversations : {
-				english : ["I don't understand", "word2", ""],
-				spanish : ["No entiendo", "spword1", "spword2"],
-				images : ["../images/word1", "../images/word2", "../images/word3"],
-				description : ["description1", "description2", "description3"]	
+				english : ["I don't understand", "I have reservations", "How much is it?", "Which direction?"],
+				spanish : ["No entiendo", "Tego reservaciones", "¿Cuánto cuesta?", "¿Por dónde?"],
+				images : ["../images/", "../images/word2", "../images/word3"],
+				description : ["'No comprendo' also means 'I do not understand'", "description2", "description3"]	
 				
 			}
 		},
@@ -51,15 +51,15 @@
 				description : ["ordenar is more common. 'Pedir' also means 'to ask for'", "'tomar' is used more commonly than 'beber'", "Both words mean same thing. Speakers in different region will use one or the other"]	
 				
 			},
-			phrases : {english : ["You welcome", "Excuse me", "Do you speak Spanish?"],
-				spanish : ["De nada", "Perdon", "Hablo un poquito"],
+			phrases : {english : ["I dont want", "How much?", "Repeat, please."],
+				spanish : ["No quiero", "¿Cuánto cuesta?", "Repita, por favor."],
 				images : ["../images/youre-welcome.gif", "../images/excuseme.jpg", "../images/doUSpeakSpanish.jpg"],
 				description : ["description1", "If you need to get by someone or leave a group of people: con permiso", "description3"]	
 				
 			},
 			conversations : {
-				english : ["word1", "word2", "Do you speak Spanish?"],
-				spanish : ["spword1", "spword1", "¿Hablo un poquito?"],
+				english : ["I need to go to the store", "What's your name?", "Do you speak Spanish?"],
+				spanish : ["Necesito ir a la tienda", "¿Cómo se llama?", "¿Hablo un poquito?"],
 				images : ["../images/word1", "../images/word2", "../images/word3"],
 				description : ["description1", "description2", "description3"]	
 				
