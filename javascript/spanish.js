d4dffc87fce750e5af007b47b0b5ef3cd491c6c1
Chayanika Ghosh <chayanika.ghosh24588@gmail.com>
2012-01-31 04:33:01 +0530

    images resized

--- javascript/spanish.js
+++ javascript/spanish.js
@@ -4,19 +4,19 @@
 			words : {
 				english : ["Business plan", "to organize", "revenue"],
 				translated : ["plan de negocios", "organizar", "ingresos"],
-				images : ["business-plan.jpg", "how-to-organise.jpg", "Revenue.jpg"],
+				images : ["business-plan2.jpg", "how-to-organise2.jpg", "Revenue2.jpg"],
 				description : [" ", " ", " "]	
 			},
 			phrases : {
 				english : ["pleased to meet you (m)", "How's it going?", "How are you today?"],
 				translated : ["encantado", "¿Cómo te va?", "Cómo está hoy?"],
-				images : ["pleaseToMeetYou.jpg", "howzitgoing.jpg", "howareYouToday.jpg"],
+				images : ["pleaseToMeetYou2.jpg", "howzitgoing2.jpg", "howareYouToday2.jpg"],
 				description : ["'pleased to meet you (f): encantada'", " ", " "]		
 			},
 			conversations : {
 				english : ["Let me introduce myself. My name is...", "Thank you for your business.", "I want to introduce you to a female friend. John, this is Mary.", "We should prepare for the worst."],
 				translated : ["Déjame presentarme. Mi nombre es...", "Apreciamos hacer negocios con usted.", "Te quiero presentar a una amiga. Juan, te presento a Maria.", "Debemos prepararnos para lo peor."],
-				images : ["Letmeintoducemyself.jpg", "Thank-you-for-your-Business.jpg", "introduce-female-friend.jpg", "WeShouldbepreparedfortheWorst.jpg"],
+				images : ["Letmeintoducemyself2.jpg", "Thank-you-for-your-Business2.jpg", "introduce-female-friend2.jpg", "WeShouldbepreparedfortheWorst2.jpg"],
 				description : [" ", " ", " ", " "]		
 			}
 		},
@@ -24,21 +24,21 @@
 			words : {
 				english : ["Soup", "Water", "Juice"],
 				translated : ["Sopa", "Agua", "Jugo"],
-				images : ["soup.jpg", "water.jpg", "Juice.jpg"],
+				images : ["soup2.jpg", "water2.jpg", "Juice2.jpg"],
 				description : [" ", " ", " "]	
 				
 			},
 			phrases : {
 				english : ["Place", "Where is the subway?", "Which direction?"],
 				translated : ["Lugar", "¿Dónde está el metro?", "¿En qué dirección?"],
-				images : ["place.jpg", "subway.jpg", "whichdirection1.jpg"],
+				images : ["place2.jpg", "subway2.jpg", "whichdirection2.jpg"],
 				description : [" ", " ", " "]	
 				
 			},
 			conversations : {
 				english : ["I don't understand", "I have reservations", "How much is it?", "Is the tip included?"],
 				translated : ["No entiendo", "Tego reservaciones", "¿Cuánto cuesta?", "¿Está la propina incluída?"],
-				images : ["idontunderstand.jpg", "i_have_reservation.jpg", "HowMuchIsIt.gif", "tip.gif"],
+				images : ["idontunderstand2.jpg", "i_have_reservation2.jpg", "HowMuchIsIt2.gif", "tip.gif"],
 				description : ["'No comprendo' also means 'I do not understand'", " ", " ", " "]	
 				
 			}
@@ -47,21 +47,21 @@
 			words : {
 				english : ["I want", "I need", "to go", "to order", "to drink", "to cook"],
 				translated : ["(Yo) Quiero", "Yo necesito", "ir", "ordenar/pedir", "tomar/beber", "cocinar/cocer"],
-				images : ["i-want-my-toy-back.jpg", "i-need.jpg", "togo1.jpg", "toOrder.jpg", "toDrink.jpg", "toCook.jpg"],
+				images : ["i-want-my-toy-back2.jpg", "i-need2.jpg", "togo2.jpg", "toOrder2.jpg", "toDrink2.jpg", "toCook2.jpg"],
 				description : ["You must have notice (Yo) which means 'I' is in paranthesis. In Spanish (Yo) is optional and is used only when emphasized. It is been generally left out in everyday speech", " ", "The pronunciation of 'r' is like 'd' in Spanish", "ordenar is more common. 'Pedir' also means 'to ask for'", "'tomar' is used more commonly than 'beber'", "Both words mean same thing. Speakers in different region will use one or the other"]
 				
 			},
 			phrases : {
 				english : ["I dont want", "How much?", "Repeat, please.", "Excuse me"],
 				translated : ["No quiero", "¿Cuánto cuesta?", "Repita, por favor.", "Perdone"],
-				images : ["IDontWant.jpg", "how-much.jpg", "repeatPlease.gif", "excuseme1.cms"],
+				images : ["IDontWant2.jpg", "how-much2.jpg", "repeatPlease2.gif", "excuseme1.cms"],
 				description : [" ", " ", " ", "Perdone(begging pardon), Disculpe(getting attention), con permiso(If you need to get by someone or leave a group of people)"]	
 				
 			},
 			conversations : {
 				english : ["I need to go to the store", "What's your name?", "Do you speak Spanish?"],
 				translated : ["Necesito ir a la tienda", "¿Cómo se llama?", "¿Hablo un poquito?"],
-				images : ["store.jpg", "whatIsYourName.jpg", "DoYouSpeakSpanish.jpg"],
+				images : ["store2.jpg", "whatIsYourName2.jpg", "DoYouSpeakSpanish2 .jpg"],
 				description : [" ", " ", " "]	
 				
 			}
