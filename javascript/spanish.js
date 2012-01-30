e11315ad70343980d600ded436d846a6dc6bc48f
Chayanika Ghosh <chayanika.ghosh24588@gmail.com>
2012-01-30 17:33:42 +0530

    finishing of business.html and similar pages

--- javascript/spanish.js
+++ javascript/spanish.js
@@ -4,7 +4,7 @@
 			words : {
 				english : ["Business plan", "to organize", "revenue"],
 				translated : ["plan de negocios", "organizar", "ingresos"],
-				images : ["business-plan.jpg", "toorganize.jpg", "Revenue.jpg"],
+				images : ["business-plan.jpg", "how-to-organise.jpg", "Revenue.jpg"],
 				description : [" ", " ", " "]	
 			},
 			phrases : {
@@ -17,7 +17,7 @@
 				english : ["Let me introduce myself. My name is...", "Thank you for your business.", "I want to introduce you to a female friend. John, this is Mary.", "We should prepare for the worst."],
 				translated : ["Déjame presentarme. Mi nombre es...", "Apreciamos hacer negocios con usted.", "Te quiero presentar a una amiga. Juan, te presento a Maria.", "Debemos prepararnos para lo peor."],
 				images : ["Letmeintoducemyself.jpg", "Thank-you-for-your-Business.jpg", "introduce-female-friend.jpg", "WeShouldbepreparedfortheWorst.jpg"],
-				description : ["", "", ""]		
+				description : [" ", " ", " ", " "]		
 			}
 		},
 		tourist : {
@@ -39,7 +39,7 @@
 				english : ["I don't understand", "I have reservations", "How much is it?", "Is the tip included?"],
 				translated : ["No entiendo", "Tego reservaciones", "¿Cuánto cuesta?", "¿Está la propina incluída?"],
 				images : ["idontunderstand.jpg", "i_have_reservation.jpg", "HowMuchIsIt.gif", "tip.gif"],
-				description : ["'No comprendo' also means 'I do not understand'", " ", " "]	
+				description : ["'No comprendo' also means 'I do not understand'", " ", " ", " "]	
 				
 			}
 		},
@@ -47,15 +47,15 @@
 			words : {
 				english : ["I want", "I need", "to go", "to order", "to drink", "to cook"],
 				translated : ["(Yo) Quiero", "Yo necesito", "ir", "ordenar/pedir", "tomar/beber", "cocinar/cocer"],
-				images : ["i-want-my-toy-back.jpg", "i-need.jpg", "togo.jpg", "toOrder.jpg", "toDrink.jpg", "toCook.jpg"],
+				images : ["i-want-my-toy-back.jpg", "i-need.jpg", "togo1.jpg", "toOrder.jpg", "toDrink.jpg", "toCook.jpg"],
 				description : ["You must have notice (Yo) which means 'I' is in paranthesis. In Spanish (Yo) is optional and is used only when emphasized. It is been generally left out in everyday speech", " ", "The pronunciation of 'r' is like 'd' in Spanish", "ordenar is more common. 'Pedir' also means 'to ask for'", "'tomar' is used more commonly than 'beber'", "Both words mean same thing. Speakers in different region will use one or the other"]
 				
 			},
 			phrases : {
 				english : ["I dont want", "How much?", "Repeat, please.", "Excuse me"],
 				translated : ["No quiero", "¿Cuánto cuesta?", "Repita, por favor.", "Perdone"],
-				images : ["IDontWant.gif", "how-much.jpg", "repeatPlease.gif", ""],
-				description : [" ", " ", " ", "Perdone(begging pardon)/ Disculpe(getting attention), con permiso(If you need to get by someone or leave a group of people)"]	
+				images : ["IDontWant.jpg", "how-much.jpg", "repeatPlease.gif", "excuseme1.cms"],
+				description : [" ", " ", " ", "Perdone(begging pardon), Disculpe(getting attention), con permiso(If you need to get by someone or leave a group of people)"]	
 				
 			},
 			conversations : {
